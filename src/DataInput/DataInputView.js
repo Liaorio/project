84a88e9458b06b1c71ad788e0c543f42987d3b77
Ao Li <liaorio@gmail.com>
2019-03-21 18:55:33 -0400

    try leaflet

--- src/DataInput/DataInputView.js
+++ src/DataInput/DataInputView.js
@@ -1,19 +1,47 @@
 import React from "react";
+import {  Map, TileLayer, Rectangle } from 'react-leaflet';
 
-import { Input } from 'antd';
+const outer = [[50.505, -29.09], [52.505, 29.09]]
+const inner = [[49.505, -2.09], [53.505, 2.09]]
+
 
 export default class DataInputView extends React.Component {
 
-    handleInput(e) {
-        let newValue = e.target.value;
-        this.props.handleInputData(newValue);
+    constructor(props) {
+        super(props);
+        this.state = {
+            bounds: outer,
+          }
     }
+    onClickInner = () => {
+        this.setState({ bounds: inner })
+      }
+    
+      onClickOuter = () => {
+        this.setState({ bounds: outer })
+      }
 
-    render() {
-        let value = this.props.data;
-
+      render() {
         return (
-            <Input placeholder="Input" onChange={(e) => this.handleInput(e)} value={value} style={{width: '150px'}} />
-        );
-    }
+            <div style={{width: '200px'}}>
+                <Map bounds={this.state.bounds}>
+                    <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Rectangle
+                    bounds={outer}
+                    color={this.state.bounds === outer ? 'red' : 'white'}
+                    onClick={this.onClickOuter}
+                    />
+                    <Rectangle
+                    bounds={inner}
+                    color={this.state.bounds === inner ? 'red' : 'white'}
+                    onClick={this.onClickInner}
+                    />
+                </Map>
+            </div>
+        )
+      }
+   
 }
