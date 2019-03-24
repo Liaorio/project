9ef638ced2dad7879bdb159a228406f492f902e9
李骜 <liaorio@gmail.com>
2019-03-23 21:46:58 -0400

    indent change from 2 to 4

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -8,30 +8,28 @@ import HomePage from '../Home/Home';
 import DataInputPage from '../DataInput/DataInput';
 
 class Root extends React.Component {
-  render() {
-    return (
-      <div>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/dataInput" component={DataInputPage}></Route>
-      </div>
-    );
-  }
+	render() {
+		return ( 
+			<div>
+				<Route exact path="/" component={HomePage} /> 
+				<Route path="/dataInput" component={DataInputPage} />  
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => {
-  return {
-    data: state.data
-  }
+	return {
+		data: state.data
+	}
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-      redirect: (route) => {
-          dispatch(push(route));
-      },
-  };
+	return {
+		redirect: (route) => {
+			dispatch(push(route));
+		},
+	};
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Root));
-
-
--- src/configureStore.js
+++ src/configureStore.js
@@ -3,22 +3,22 @@ import { createLogger } from 'redux-logger'
 import { rootReducer } from './Root/rootReducer'
 
 const configureStore = preloadedState => {
-  const store = createStore(
-    rootReducer,
-    preloadedState,
-    compose(
-      applyMiddleware(createLogger()),
-    )
-  )
+	const store = createStore(
+		rootReducer,
+		preloadedState,
+		compose(
+			applyMiddleware(createLogger()),
+		)
+	)
 
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('./Root/rootReducer', () => {
-      store.replaceReducer(rootReducer)
-    })
-  }
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('./Root/rootReducer', () => {
+			store.replaceReducer(rootReducer)
+		})
+	}
 
-  return store
+	return store
 }
 
 export default configureStore
