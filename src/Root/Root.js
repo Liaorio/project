8cddad6defbed9bba1178b3e7a34a3b0e6cd4495
Ao Li <liaorio@gmail.com>
2019-03-26 12:32:18 -0400

    tweak for build

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -4,15 +4,15 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 
-import HomePage from '../Home/Home';
+// import HomePage from '../Home/Home';
 import DataInputPage from '../DataInput/DataInput';
 
 class Root extends React.Component {
 	render() {
 		return ( 
 			<Router>
-				<Route exact path="/" component={HomePage} /> 
-				<Route path="/dataInput" component={DataInputPage} />  
+				<Route exact path="/" component={DataInputPage} /> 
+				{/* <Route path="/dataInput" component={DataInputPage} />   */}
 			</Router>
 		);
 	}
